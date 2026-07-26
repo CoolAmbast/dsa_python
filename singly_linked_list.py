331f4ca8f983b50d53202ec78e2e111f3da1660a
Cool Ambast <83899058+CoolAmbast@users.noreply.github.com>
2026-07-27 01:22:27 +0530

    linked list prepend method/addition of value from head to tail

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -35,3 +35,32 @@ class LinkedList:
             count+=1
             current = current.next_node
         return count
+    
+    def add(self,data):
+        """
+        Adds a new node containing data at the head of the list
+        This operation takes O(1) time
+        """
+        
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+        
+    def __repr__(self):
+        
+        """
+        Returns a string representation of the list
+        """
+        
+        nodes = []
+        current = self.head
+        
+        while current:
+            if current is self.head:
+                nodes.append("Head: %s" % current.data)
+            elif current.next_node is None:
+                nodes.append("Tail: %s" % current.data)
+            else:
+                nodes.append("[%s]"% current.data)
+            current = current.next_node
+        return '-> '.join(nodes)
